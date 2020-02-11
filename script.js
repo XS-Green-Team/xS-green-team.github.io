f71ca75c1c93b951018d9c335781ecdda8a1e16f
Adrian Serapio <adtserapio@gmail.com>
2020-02-12 04:04:17 +0800

    Changed logic

--- script.js
+++ script.js
@@ -47,20 +47,20 @@ async function runModel(file) {
     img_tensor = tf.div(img_tensor, 255);
     let predictions = await model.predict(img_tensor).dataSync();
 
-    var largest = predictions[0];
-    var largest_class = 0;
+    // var largest = predictions[0];
+    // var largest_class = 0;
 
-    for (var i = 0; i < predictions.length; i++) {
-        if (largest < predictions[i] ) {
-        largest = predictions[i];
-        largest_class = i;
-        }
-    }
+    // for (var i = 0; i < predictions.length; i++) {
+    //     if (largest < predictions[i] ) {
+    //     largest = predictions[i];
+    //     largest_class = i;
+    //     }
+    // }
 
 
      document.getElementById('loader').style.display = 'none'; 
 
-     document.getElementById("prediction-text").innerHTML = largest_class;
+     document.getElementById("prediction-text").innerHTML = predictions;
 
 
     // if (percent > 50) {
